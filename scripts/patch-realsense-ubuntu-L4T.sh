a55c87a9286fd1f8cef78aa4c829a216f5c8b7bf
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-30 22:13:55 +1100

    Hard-coded changes to patch-realsense-L4T.sh
    These changes are targeting a TX2, JetPack 4.6.1. Ubuntu 20.

--- scripts/patch-realsense-ubuntu-L4T.sh
+++ scripts/patch-realsense-ubuntu-L4T.sh
@@ -22,17 +22,18 @@ fi
 source ./scripts/patch-utils.sh
 
 # Get the required tools to build the patched modules
-sudo apt-get install build-essential git -y
+#sudo apt-get install build-essential git -y
 
 #Activate fan to prevent overheat during KM compilation
-if [ -f /sys/devices/pwm-fan/target_pwm ]; then
-	echo 200 | sudo tee /sys/devices/pwm-fan/target_pwm || true
-fi
+#if [ -f /sys/devices/pwm-fan/target_pwm ]; then
+	#echo 200 | sudo tee /sys/devices/pwm-fan/target_pwm || true
+#fi
 
 #Tegra-specific
 KERNEL_RELEASE="4.9"
 #Identify the Jetson board
-JETSON_BOARD=$(tr -d '\0' </proc/device-tree/model)
+#JETSON_BOARD=$(tr -d '\0' </proc/device-tree/model)
+JETSON_BOARD="quill"
 echo -e "\e[32mJetson Board (proc/device-tree/model): ${JETSON_BOARD}\e[0m"
 
 JETSON_L4T=""
@@ -49,22 +50,7 @@ else
 	exit;
 fi
 
-PATCHES_REV=""
-#Select the kernel patches revision that matches the paltform configuration
-case ${JETSON_L4T_VERSION} in
-
-  "32.2.1" | "32.2.3" | "32.3.1" | "32.4.3")
-    PATCHES_REV="4.4"		# Baseline for the patches
-    echo -e "\e[32mNote: the patch makes changes to kernel device tree to support HID IMU sensors\e[0m"
-    ;;
-  "32.4.4" | "32.5" | "32.5.1" | "32.6.1")
-    PATCHES_REV="4.4.1"	# JP 4.4.1
-    ;;
-  *)
-    echo -e "\e[41mUnsupported JetPack revision ${JETSON_L4T_VERSION} aborting script\e[0m"
-    exit;
-    ;;
-esac
+PATCHES_REV="4.4.1"	# JP 4.4.1
 echo -e "\e[32mL4T ${JETSON_L4T_VERSION} to use patches revision ${PATCHES_REV}\e[0m"
 
 
@@ -117,16 +103,9 @@ echo -e "\e[32mPrepare workspace for kernel build\e[0m"
 sudo make ARCH=arm64 mrproper -j$(($(nproc)-1)) && sudo make ARCH=arm64 tegra_defconfig -j$(($(nproc)-1))
 
 #Reuse existing module.symver
-kernel_ver=`uname -r`
+kernel_ver='4.9.253-tegra'
 sudo cp /usr/src/linux-headers-${kernel_ver}-ubuntu18.04_aarch64/kernel-4.9/Module.symvers .
 
-#Jetpack prior to 4.4.1 requires manual reconfiguration of kernel
-if [ "4.4" = "$PATCHES_REV" ]; then
-	echo -e "\e[32mUpdate the kernel tree to support HID IMU sensors\e[0m"
-	sudo sed -i '/CONFIG_HID_SENSOR_ACCEL_3D/c\CONFIG_HID_SENSOR_ACCEL_3D=m' .config
-	sudo sed -i '/CONFIG_HID_SENSOR_GYRO_3D/c\CONFIG_HID_SENSOR_GYRO_3D=m' .config
-	sudo sed -i '/CONFIG_HID_SENSOR_IIO_COMMON/c\CONFIG_HID_SENSOR_IIO_COMMON=m\nCONFIG_HID_SENSOR_IIO_TRIGGER=m' .config
-fi
 sudo make ARCH=arm64 prepare modules_prepare  -j$(($(nproc)-1))
 
 #Remove previously applied patches
@@ -134,9 +113,6 @@ sudo git reset --hard
 echo -e "\e[32mApply Librealsense Kernel Patches\e[0m"
 sudo -s patch -p1 < ./LRS_Patches/01-realsense-camera-formats-L4T-${PATCHES_REV}.patch
 sudo -s patch -p1 < ./LRS_Patches/02-realsense-metadata-L4T-${PATCHES_REV}.patch
-if [ "4.4" = "$PATCHES_REV" ]; then # for Jetpack 4.4 and older
-	sudo -s patch -p1 < ./LRS_Patches/03-realsense-hid-L4T-4.9.patch
-fi
 sudo -s patch -p1 < ./LRS_Patches/04-media-uvcvideo-mark-buffer-error-where-overflow.patch
 sudo -s patch -p1 < ./LRS_Patches/05-realsense-powerlinefrequency-control-fix.patch
 
@@ -146,49 +122,19 @@ sudo -s make -j$(($(nproc)-1)) ARCH=arm64 M=drivers/media/usb/uvc/ modules
 echo -e "\e[32mCompiling v4l2-core modules\e[0m"
 #sudo -s make -j -C $KBASE M=$KBASE/drivers/media/v4l2-core modules
 sudo -s make -j$(($(nproc)-1)) ARCH=arm64  M=drivers/media/v4l2-core modules
-if [ "4.4" = "$PATCHES_REV" ]; then # for Jetpack 4.4 and older
-	echo -e "\e[32mCompiling accelerometer and gyro modules\e[0m"
-	sudo -s make -j$(($(nproc)-1)) ARCH=arm64  M=drivers/iio modules
-fi
 
 echo -e "\e[32mCopying the patched modules to (~/) \e[0m"
 sudo cp drivers/media/usb/uvc/uvcvideo.ko ~/${TEGRA_TAG}-uvcvideo.ko
 sudo cp drivers/media/v4l2-core/videobuf-vmalloc.ko ~/${TEGRA_TAG}-videobuf-vmalloc.ko
 sudo cp drivers/media/v4l2-core/videobuf-core.ko ~/${TEGRA_TAG}-videobuf-core.ko
-if [ "4.4" = "$PATCHES_REV" ]; then # for Jetpack 4.4 and older
-	sudo cp drivers/iio/common/hid-sensors/hid-sensor-iio-common.ko ~/${TEGRA_TAG}-hid-sensor-iio-common.ko
-	sudo cp drivers/iio/common/hid-sensors/hid-sensor-trigger.ko ~/${TEGRA_TAG}-hid-sensor-trigger.ko
-	sudo cp drivers/iio/accel/hid-sensor-accel-3d.ko ~/${TEGRA_TAG}-hid-sensor-accel-3d.ko
-	sudo cp drivers/iio/gyro/hid-sensor-gyro-3d.ko ~/${TEGRA_TAG}-hid-sensor-gyro-3d.ko
-fi
 popd
 
 echo -e "\e[32mMove the modified modules into the modules tree\e[0m"
-if [ "4.4" = "$PATCHES_REV" ]; then # for Jetpack 4.4 and older
-#Optional - create kernel modules directories in kernel tree
-	sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/iio/accel
-	sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/iio/gyro
-	sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/iio/common/hid-sensors
-	sudo cp  ~/${TEGRA_TAG}-hid-sensor-accel-3d.ko     /lib/modules/`uname -r`/kernel/drivers/iio/accel/hid-sensor-accel-3d.ko
-	sudo cp  ~/${TEGRA_TAG}-hid-sensor-gyro-3d.ko      /lib/modules/`uname -r`/kernel/drivers/iio/gyro/hid-sensor-gyro-3d.ko
-	sudo cp  ~/${TEGRA_TAG}-hid-sensor-iio-common.ko   /lib/modules/`uname -r`/kernel/drivers/iio/common/hid-sensors/hid-sensor-iio-common.ko
-	sudo cp  ~/${TEGRA_TAG}-hid-sensor-trigger.ko      /lib/modules/`uname -r`/kernel/drivers/iio/common/hid-sensors/hid-sensor-trigger.ko
-fi
 
 # update kernel module dependencies
 sudo depmod
 
 echo -e "\e[32mInsert the modified kernel modules\e[0m"
-try_module_insert uvcvideo              ~/${TEGRA_TAG}-uvcvideo.ko                /lib/modules/`uname -r`/kernel/drivers/media/usb/uvc/uvcvideo.ko
-if [ "4.4" = "$PATCHES_REV" ]; then # for Jetpack 4.4 and older
-	try_module_insert hid_sensor_accel_3d   ~/${TEGRA_TAG}-hid-sensor-accel-3d.ko     /lib/modules/`uname -r`/kernel/drivers/iio/accel/hid-sensor-accel-3d.ko
-	try_module_insert hid_sensor_gyro_3d    ~/${TEGRA_TAG}-hid-sensor-gyro-3d.ko      /lib/modules/`uname -r`/kernel/drivers/iio/gyro/hid-sensor-gyro-3d.ko
-	#Preventively unload all HID-related modules
-	try_unload_module hid_sensor_accel_3d
-	try_unload_module hid_sensor_gyro_3d
-	try_unload_module hid_sensor_trigger
-	try_unload_module hid_sensor_trigger
-	try_module_insert hid_sensor_trigger    ~/${TEGRA_TAG}-hid-sensor-trigger.ko      /lib/modules/`uname -r`/kernel/drivers/iio/common/hid-sensors/hid-sensor-trigger.ko
-	try_module_insert hid_sensor_iio_common ~/${TEGRA_TAG}-hid-sensor-iio-common.ko   /lib/modules/`uname -r`/kernel/drivers/iio/common/hid-sensors/hid-sensor-iio-common.ko
-fi
+try_module_insert uvcvideo              ~/${TEGRA_TAG}-uvcvideo.ko                /lib/modules/${kernel_ver}/kernel/drivers/media/usb/uvc/uvcvideo.ko
+
 echo -e "\e[92m\n\e[1mScript has completed. Please consult the installation guide for further instruction.\n\e[0m"
